0ab737e373e2499ed93672aa2c5ce13d7427406a
blueprint00 <ayun96@naver.com>
2020-12-21 23:36:24 +0900

    StoneBridge

--- BinarySearch/StoneBridge.java
+++ BinarySearch/StoneBridge.java
@@ -28,7 +28,13 @@ public class StoneBridge {
         	
         	if(distance - prev < mid) remove ++;
         	
-        	if(remove <= n) {
+        	if(remove <= n) { 
+        		// 정확히 n개가 아니여도 되는 이유는 미처 지우지 못한 바위의 개수만큼 더 지워도 바위 간 간격의 최솟값이 기준값인 x보다 작을 수는 없으므로 상관없기 때문입니다.
+        		// if remove == 2 && mid == 3
+        		// 거리는 4, 2, 7, 7, 7, 7
+        		// 4를 지우면 7 중 어느걸 지워도 min == 2 이다.
+        		// 즉 remove == 1 이어도 min 간격과는 상관 없다!
+        		// 뭔 말 인지 알겠니..?
         		answer = mid > answer? mid : answer;
         		first = mid + 1;
         	} else {
