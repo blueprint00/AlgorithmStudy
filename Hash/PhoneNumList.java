d370cd5d848f46ea42569c3474f3721ad0ab15bd
blueprint00 <ayun96@naver.com>
2021-03-20 21:36:55 +0900

    210320

--- Hash/PhoneNumList.java
+++ Hash/PhoneNumList.java
@@ -8,34 +8,43 @@ public class PhoneNumList {
 	}
 	
     public static boolean solution(String[] phone_book) {
-    	boolean answer = true;
-    	
 //    	for(int i = 0; i < phone_book.length; i ++) {
 //    		for(int j = i + 1; j < phone_book.length; j ++) {
 //    			if(phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i])) {
-//    				answer = false;
-//    				break;
+//    				return false;
 //    			}
 //    		}
 //    	}
     	
+		// for(String phone : phone_book){
+		// 	map.put(phone, phone);
+		// }
+    	// for(int i = 0; i < phone_book.length; i ++) {
+    	// 	String phone = map.get(phone_book[i]);
+    	// 	for(int j = i + 1; j < phone_book.length; j ++) {
+    	// 		if(phone.startsWith(phone_book[j]) || phone_book[j].startsWith(phone)) {
+    	// 			return false;
+    	// 		}
+    	// 	}
+    	// }
+
+		// 210320
+		Arrays.sort(phone_book, new Comparator<String>() {
+			public int compare(String o1, String o2){
+				return o2.length() - o1.length();
+			}
+		});
+
     	HashMap<String, String> map = new HashMap<>();
-    	
     	for(String phone : phone_book) {
-    		map.put(phone, phone);
+			map.put(phone, phone);
+			if(map.get(phone) != null) return false;
+    		for(int i = 1, len = phone.length(); i <= len; i ++){
+				map.put(phone.substring(0, i), "");
+			}
     	}
     	
-    	for(int i = 0; i < phone_book.length; i ++) {
-    		String phone = map.get(phone_book[i]);
-    		for(int j = i + 1; j < phone_book.length; j ++) {
-    			if(phone.startsWith(phone_book[j]) || phone_book[j].startsWith(phone)) {
-    				answer = false;
-    				break;
-    			}
-    		}
-    	}
-    	
-    	return answer;
+    	return true;
     }
 
 }
