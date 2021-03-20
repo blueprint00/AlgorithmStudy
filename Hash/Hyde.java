cb05f6ea2d66a9196ec892a8b4eb3ee87bd1f7d6
blueprint00 <ayun96@naver.com>
2021-03-20 21:36:44 +0900

    210320

--- Hash/Hyde.java
+++ Hash/Hyde.java
@@ -11,19 +11,31 @@ public class Hyde {
 	}
 	
     public static int solution(String[][] clothes) {
-    	int answer = 1;
+    	// int answer = 1;
     	
-    	HashMap<String, Integer> map = new HashMap<>();
+    	// HashMap<String, Integer> map = new HashMap<>();
     	
-    	for(String[] cloth : clothes) { // 2차 배열 나누
-    		System.out.println("1 " + cloth[0] + " " + cloth[1]);
-    		map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1); // 종류별로 존재하지 않을 경우 0, 아니면 +1
-    	}
+    	// for(String[] cloth : clothes) { // 2차 배열 나누rl
+    	// 	System.out.println("1 " + cloth[0] + " " + cloth[1]);
+    	// 	map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1); // 종류별로 존재하지 않을 경우 0, 아니면 +1
+    	// }
     	
-    	for(String key : map.keySet()) {
-    		answer *= (map.get(key) + 1); // 조합 경우의 수 구하기
-    	}
+    	// for(String key : map.keySet()) {
+    	// 	answer *= (map.get(key) + 1); // 조합 경우의 수 구하기
+    	// }
     	
-    	return answer - 1; // 전체 경우에서 하나도 선택 안하는거 -1
+    	// return answer - 1; // 전체 경우에서 하나도 선택 안하는거 -1
+
+		//210320
+		int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+        for(String key : map.keySet()){
+            answer *= (map.get(key) + 1);
+        }
+        return answer - 1;
     }
 }
