cb0c2b5c8f5de526fc6457c427b8fb5a6ec66ee8
blueprint00 <ayun96@naver.com>
2021-03-24 16:38:37 +0900

    210324

---
+++ Heap/Spicy.java
@@ -0,0 +1,43 @@
+package Heap;
+import java.util.*;
+
+public class Spicy {
+	public static void main(String[] args) {
+		int[] scoville = {1, 2, 3, 9, 10, 12};
+		int K = 7;
+		
+		System.out.println(solution(scoville, K));
+	}
+	
+    public static int solution(int[] scoville, int K) {
+		//210324
+		int answer = 0;
+
+		PriorityQueue<Integer> heap = new PriorityQueue<>();
+		for(int sco : scoville) heap.offer(sco);
+
+		while(heap.peek() <= K){
+			if(heap.size() == 1) return -1;
+
+			int result = heap.poll() + heap.poll() * 2;
+			heap.offer(result);
+			answer ++;
+		}
+		return answer;
+		
+    	// int answer = 0;
+    	
+    	// PriorityQueue<Integer> heap = new PriorityQueue<>(); 
+    	// for(int sco : scoville) heap.offer(sco);
+    	
+    	// while(heap.peek() <= K) {
+    	// 	if(heap.size() == 1) return -1;
+    		
+    	// 	int result = (heap.poll() + (heap.poll() * 2));
+    	// 	heap.offer(result);
+    	// 	answer ++;
+    	// }
+    	
+    	// return answer;
+    }
+}
