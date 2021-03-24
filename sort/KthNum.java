7620675446f1e116b6697d6597e2c3651e07ab07
blueprint00 <ayun96@naver.com>
2021-03-24 22:42:23 +0900

    210324

--- sort/KthNum.java
+++ sort/KthNum.java
@@ -3,21 +3,36 @@ import java.util.*;
 
 public class KthNum {
     public int[] solution(int[] array, int[][] commands) {
+        //210324
         int[] answer = new int[commands.length];
-
         for(int i = 0; i < commands.length; i ++){
-        	int len = commands[i][1] - commands[i][0] + 1;
-        	int[] tmp = new int[len];
-            int k = 0;
-        	
-            for(int j = commands[i][0] - 1; j < commands[i][1]; j ++){
-                tmp[k ++] = array[j];
+            int len = commands[i][1] - commands[i][1] + 1;
+            int[] tmp = new int[len];
+            int idx = 0;
+            for(int j = commands[i][0]; j < commands[i][1]; j ++){
+                tmp[idx ++] = array[j];
             }
-            
+
             Arrays.sort(tmp);
             answer[i] = tmp[commands[i][2] - 1];
         }
-        
         return answer;
+
+        // int[] answer = new int[commands.length];
+
+        // for(int i = 0; i < commands.length; i ++){
+        // 	int len = commands[i][1] - commands[i][0] + 1;
+        // 	int[] tmp = new int[len];
+        //     int k = 0;
+        	
+        //     for(int j = commands[i][0] - 1; j < commands[i][1]; j ++){
+        //         tmp[k ++] = array[j];
+        //     }
+            
+        //     Arrays.sort(tmp);
+        //     answer[i] = tmp[commands[i][2] - 1];
+        // }
+        
+        // return answer;
 	}
 }
