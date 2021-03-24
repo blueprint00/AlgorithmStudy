3e5c673622af39247698774d26167bc38decc021
blueprint00 <ayun96@naver.com>
2021-03-24 22:42:30 +0900

    210324

--- sort/MaxNum.java
+++ sort/MaxNum.java
@@ -3,25 +3,46 @@ import java.util.*;
 
 public class MaxNum {
 	public String solution(int[] numbers) {
+        //210324   
         String answer = "";
-        
-		int len = numbers.length;
-        
+        int len = numbers.length;
+
         String[] str_numbers = new String[len];
-        for(int i = 0; i < len; i ++) {
-        	str_numbers[i] = String.valueOf(numbers[i]);
+        for(int i = 0; i < len; i ++){
+            str_numbers[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(str_numbers, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return (o2 + o1).compareTo(o1 + o2);
-			}
+        Arrays.sort(str_numbers, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                return (o2 + o1).compareTo(o1 + o2); // 내림차순
+            }    
         });
         
-        for(int i = 0; i < str_numbers.length; i ++)
-            answer += str_numbers[i];
-        
+        for(String str : str_numbers){
+            answer += str;
+        }
+
         return answer.charAt(0) == '0' ? "0" : answer;
+        
+        // String answer = "";
+        
+		// int len = numbers.length;
+        
+        // String[] str_numbers = new String[len];
+        // for(int i = 0; i < len; i ++) {
+        // 	str_numbers[i] = String.valueOf(numbers[i]);
+        // }
+        
+        // Arrays.sort(str_numbers, new Comparator<String>() {
+		// 	@Override
+		// 	public int compare(String o1, String o2) {
+		// 		return (o2 + o1).compareTo(o1 + o2);
+		// 	}
+        // });
+        
+        // for(int i = 0; i < str_numbers.length; i ++)
+        //     answer += str_numbers[i];
+        
+        // return answer.charAt(0) == '0' ? "0" : answer;
     }
 }
