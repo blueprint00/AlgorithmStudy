23485b641417a5952e6c97428da9fa6ffda9ae70
blueprint00 <ayun96@naver.com>
2021-03-24 22:57:05 +0900

    210324

--- BruteForce/Carpet.java
+++ BruteForce/Carpet.java
@@ -9,15 +9,22 @@ public class Carpet {
 			System.out.println(ans);
 	}
 	public static int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        
-       int w, h;
-       double square = Math.sqrt(Math.pow((brown + 4) / 2, 2) - 4 * (brown + yellow));
+        //210324
+        int w, h;
+        double square = Math.sqrt(Math.pow((brown + 4) / 2, 2) - 4 * (brown + yellow));
         w = (int) ((brown + 4) / 2 + square) / 2;
         h = (int) ((brown + 4) / 2 - square) / 2;
-        answer[0] = w;
-        answer[1] = h;
+
+        return new int[] {w, h};
+
+    //     int[] answer = new int[2];
+    //    int w, h;
+    //    double square = Math.sqrt(Math.pow((brown + 4) / 2, 2) - 4 * (brown + yellow));
+    //     w = (int) ((brown + 4) / 2 + square) / 2;
+    //     h = (int) ((brown + 4) / 2 - square) / 2;
+    //     answer[0] = w;
+    //     answer[1] = h;
         
-        return answer;
+    //     return answer;
     }
 }
