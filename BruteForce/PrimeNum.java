6260e1f627470e39067642ddeb15bf08d663622f
blueprint00 <ayun96@naver.com>
2021-03-24 22:57:14 +0900

    210324

--- BruteForce/PrimeNum.java
+++ BruteForce/PrimeNum.java
@@ -7,34 +7,61 @@ public class PrimeNum {
 		System.out.println(solution(numbers));
 	}
 	
-	public static int solution(String numbers) {
-        int answer = 0;
-    	TreeSet<Integer> set = new TreeSet<>(); // 중복 제거, 정렬
-      
-        //순열 알고리즘....
-        permutation("", numbers, set);
-        
-        for(Integer num : set) {
-        	if(num > 1 && isPrime(num)) answer ++;
-        	System.out.println(num + " " +isPrime(num));
-        }
-        
-        return answer;
-	}
-	
-	public static void permutation(String prefix, String str, TreeSet<Integer> set) {
+	//210324
+	public static void permutation(String prefix, String str, TreeSet<Integer> set){
 		int len = str.length();
-		if(!prefix.equals("")) { set.add(Integer.valueOf(prefix)); }
-		
-		for(int i = 0; i < len; i ++) {
-			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, len), set);
+		if(!prefix.equals("")) {
+			set.add(Integer.valueOf(prefix));
+		}
+
+		for(int i = 0; i < len; i ++){
+			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1), set);
 		}
 	}
-	
-	public static boolean isPrime(int num) {
-		for(int i = 2; i <= Math.sqrt(num); i ++) {
-			if(num % i == 0) return false;
+	public static boolean isPrime(int num){
+		for(int i = 2; i <= Math.sqrt(num); i ++){
+			if(num % i == 0) return false; 
 		}
 		return true;
+	}
+	public static int solution(String numbers) {
+		int answer = 0;
+		TreeSet<Integer> set = new TreeSet<>();
+
+		permutation("", numbers, set);
+
+		for(int i : set){
+			if(i > 1 && isPrime(i)) answer ++;
+		}
+		return answer;
+
+    //     int answer = 0;
+    // 	TreeSet<Integer> set = new TreeSet<>(); // 중복 제거, 정렬
+      
+    //     //순열 알고리즘....
+    //     permutation("", numbers, set);
+        
+    //     for(Integer num : set) {
+    //     	if(num > 1 && isPrime(num)) answer ++;
+    //     	System.out.println(num + " " +isPrime(num));
+    //     }
+        
+    //     return answer;
+	// }
+	
+	// public static void permutation(String prefix, String str, TreeSet<Integer> set) {
+	// 	int len = str.length();
+	// 	if(!prefix.equals("")) { set.add(Integer.valueOf(prefix)); }
+		
+	// 	for(int i = 0; i < len; i ++) {
+	// 		permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, len), set);
+	// 	}
+	// }
+	
+	// public static boolean isPrime(int num) {
+	// 	for(int i = 2; i <= Math.sqrt(num); i ++) {
+	// 		if(num % i == 0) return false;
+	// 	}
+	// 	return true;
 	}
 }
