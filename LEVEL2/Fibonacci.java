f5dd06e416e21ec726c96d24fef0cb7892287b55
blueprint00 <ayun96@naver.com>
2021-04-11 15:05:32 +0900

    210411

---
+++ LEVEL2/Fibonacci.java
@@ -0,0 +1,18 @@
+package LEVEL2;
+
+public class Fibonacci {
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(solution(n));
+    }
+    public static int solution(int n) {
+        // if(n == 0 || n == 1) return n;
+        int[] fibo = new int[100000];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i = 2; i <= n ; i ++){
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        return fibo[n] % 1234567;
+    }
+}
