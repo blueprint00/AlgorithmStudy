178a77acf5776e5426745a9ed679d9d1cd9bb72f
blueprint00 <ayun96@naver.com>
2021-04-11 15:12:04 +0900

    210411

--- LEVEL2/Fibonacci.java
+++ LEVEL2/Fibonacci.java
@@ -11,7 +11,7 @@ public class Fibonacci {
         fibo[0] = 0;
         fibo[1] = 1;
         for(int i = 2; i <= n ; i ++){
-            fibo[i] = fibo[i - 1] + fibo[i - 2];
+            fibo[i] = fibo[i - 1] % 1234567 + fibo[i - 2] % 1234567;
         }
         return fibo[n] % 1234567;
     }
